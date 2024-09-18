519c3329962f445320f1d2f92dd8d12c760ed946
Davi Rodrigues do Nascimento <estagiario.nti02@faesa.br>
2024-09-18 16:51:33 -0300

    setting up first page

---
+++ app/Http/Controllers/Site/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+class HomeController
+{
+    public function home ()
+    {
+        return view('site/home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Site\HomeController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/homepage', [HomeController::class, 'home']);
 
 Route::get('/', function () {
     return view('welcome');

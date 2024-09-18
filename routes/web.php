6e0e7f8be2173c81775b998523daf901de32a7ef
Davi Rodrigues do Nascimento <estagiario.nti02@faesa.br>
2024-09-18 17:53:31 -0300

    setting up readme.md

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Admin\SupportController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/homepage', [HomeController::class, 'home']);
+// We define a name to make references to the name
+
+Route::get('/homepage', [HomeController::class, 'home'])->name('homepage.home');
+Route::get('/support', [SupportController::class, 'index'])->name('support.index');
 
 Route::get('/', function () {
     return view('welcome');
 });
+

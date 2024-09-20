37440d7d1f0d9c346195e842d262e898dadb5a54
Davi Rodrigues do Nascimento <estagiario.nti02@faesa.br>
2024-09-20 14:36:14 -0300

    method put using Blade directives

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,11 +1,11 @@
 <h1>Edit Question {{ $support->id }}</h1>
 
 <form action="{{ route('support.update', $support->id) }}" method="POST">
-
-    <input type="text" placeholder="Subject" name="subject" value="{{ $support->subject }}">
-
     {{-- <input type="text" value="{{ csrf_token() }}" name="_token">  <!-- OBS: Do not forget name tag! Validate that the request is actually valid --> --}}
     @csrf() <!-- diretiva que cria token automaticamente --> 
+    @method('PUT') <!-- hidden information: -->
+    <input type="text" placeholder="Subject" name="subject" value="{{ $support->subject }}">
+
     
     <textarea name="body" cols="30" rows="5" placeholder="Description" name="body">{{ $support->body }}</textarea>
     

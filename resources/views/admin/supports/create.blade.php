47a9e94c3ff5a562c9bb238c898f82db9d2fae76
Davi Rodrigues do Nascimento <estagiario.nti02@faesa.br>
2024-09-20 16:14:05 -0300

    add error checking when submitting form

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,13 +1,16 @@
 <h1>New Question</h1>
 
+{{-- $errors injected automalatically inside all the views --}}
+@if ($errors->any())
+    @foreach($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+@endif
+
 <form action="{{ route('support.store') }}" method="POST">
+    @csrf()
 
     <input type="text" placeholder="Subject" name="subject">
-
-    {{-- <input type="text" value="{{ csrf_token() }}" name="_token">  <!-- OBS: Do not forget name tag! Validate that the request is actually valid --> --}}
-    @csrf() <!-- diretiva que cria token automaticamente --> 
-    
-    <textarea name="body" cols="30" rows="5" placeholder="Description" name="body"></textarea>
-    
+    <textarea name="body" cols="30" rows="5" placeholder="Description"></textarea>
     <button type="submit">Submit</button>
 </form>
